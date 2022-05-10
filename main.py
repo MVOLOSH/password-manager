8dc44d9ef73d64b47f4f1eadbd2af2d51c77e349
MVOLOSH <53019492+MVOLOSH@users.noreply.github.com>
2022-05-10 20:33:36 +0300

    save and add

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from tkinter import *
 
+
+def save_to_file():
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website_enter.get()} \t|\t {email_enter.get()} \t|\t {password_enter.get()}\n")
+        website_enter.delete(0, END)
+        email_enter.delete(0, END)
+        password_enter.delete(0, END)
+        website_enter.focus()
+
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -18,24 +28,21 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-
 website_enter = Entry(width=30)
-website_enter.grid(row=1,column=1,columnspan=2)
+website_enter.grid(row=1, column=1, columnspan=2)
+website_enter.focus()
 
 email_enter = Entry(width=30)
-email_enter.grid(row=2,column=1,columnspan=2)
+email_enter.grid(row=2, column=1, columnspan=2)
 
 password_enter = Entry(width=30)
-password_enter.grid(row=3,column=1,columnspan=2)
+password_enter.grid(row=3, column=1, columnspan=2)
 
-generate_password_button = Button(text="Generate Password",width=25)
-generate_password_button.grid(row=4,column=1,columnspan=2)
+generate_password_button = Button(text="Generate Password", width=25)
+generate_password_button.grid(row=4, column=1, columnspan=2)
 
-add_button = Button(text="Add",width=25)
-add_button.grid(row=5,column=1,columnspan=2)
-
-
-
-
+add_button = Button(text="Add", width=25,command=save_to_file)
+add_button.grid(row=5, column=1, columnspan=2)
 
 window.mainloop()
+save_to_file()
