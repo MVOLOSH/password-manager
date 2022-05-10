deab804410a65b5afd2242df2338e61e4c935eea
MVOLOSH <53019492+MVOLOSH@users.noreply.github.com>
2022-05-10 23:26:31 +0300

    search password function

--- main.py
+++ main.py
@@ -2,11 +2,28 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, shuffle, choice
 import pyperclip
+import json
+
+
+def find_password():
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error",message="File not found!")
+    else:
+        if website_enter.get() in data:
+            email = data[website_enter.get()]["Email/Username"]
+            password = data[website_enter.get()]["Password"]
+            messagebox.showinfo(title=website_enter.get(), message=f"Email/Username: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error",message=f"No info for {website_enter.get()} exists.")
+
 
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-               'v','w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-               'Q','R','S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -19,25 +36,36 @@ def generate_password():
 
     password = ''.join(password_list)
     password_enter.delete(0, END)
-    password_enter.insert(0,password)
+    password_enter.insert(0, password)
     pyperclip.copy(password)
 
 
-
 def save_to_file():
-    if len(website_enter.get()) == 0 or len(password_enter.get()) == 0:
+    new_data = {
+        website_enter.get(): {
+            "Email/Username": email_enter.get(),
+            "Password": password_enter.get()
+        }
+    }
+    if len(website_enter.get()) == 0 or len(password_enter.get()) == 0 or len(email_enter.get()) == 0:
         messagebox.showinfo(title="Oops", message="Dont leave empty fields!")
     else:
-        is_okay = messagebox.askokcancel(title="website", message=f"Email/Username: {email_enter.get()}"
-                                                                  f"\nPassword: {password_enter.get()}"
-                                                                  f"\nDo you want to save it?")
-        if is_okay:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website_enter.get()} \t|\t {email_enter.get()} \t|\t {password_enter.get()}\n")
-                website_enter.delete(0, END)
-                email_enter.delete(0, END)
-                password_enter.delete(0, END)
-                website_enter.focus()
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_enter.delete(0, END)
+            email_enter.delete(0, END)
+            password_enter.delete(0, END)
+            website_enter.focus()
 
 
 window = Tk()
@@ -68,11 +96,14 @@ email_enter.grid(row=2, column=1, columnspan=2)
 password_enter = Entry(width=30)
 password_enter.grid(row=3, column=1, columnspan=2)
 
-generate_password_button = Button(text="Generate Password", width=25,command=generate_password)
+generate_password_button = Button(text="Generate Password", width=25, command=generate_password)
 generate_password_button.grid(row=4, column=1, columnspan=2)
 
 add_button = Button(text="Add", width=25, command=save_to_file)
 add_button.grid(row=5, column=1, columnspan=2)
+
+search_button = Button(text="Search", width=25, command=find_password)
+search_button.grid(row=6, column=1, columnspan=2)
 
 window.mainloop()
 save_to_file()
